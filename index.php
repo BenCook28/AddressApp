89978fef9ca051e81e14592f50761f44549d32bc
BenCook28 <benthomascook28@gmail.com>
2021-02-03 14:00:39 -0500

    most of template

--- index.php
+++ index.php
@@ -10,10 +10,33 @@
         <h1>Birdwatching Club Address App</h1>
     </div>
     <form method="post" action="index.php">
-    <label>Enter address</label><br><br>
-    <input type="address"/>
+    <label>Street address 1</label><br><br>
+    <input type="address1"/>
     <br><br>
+
+    <label>Street address 2</label><br><br>
+    <input type="address1"/>
+    <br><br>
+
     <input type="submit" name="submit" value="Submit"/>
     </form>
+
+    <?php
+        if(isset($_GET["submit"])){
+            $address = $_Get["address"];
+        }
+
+        $request_doc_template = <<<EOT
+        <?xml version="1.0"?>
+        <AddressValidateRequest USERID="812SYLIS7409">
+            <Revision>1</Revision>
+            <Address ID=$address></Address1>
+        </AddressValidateRequest>
+        EOT;
+
+        $doc_string = preg_replace('/[\t\n]/', '', $request_doc_template);
+        $doc_string = urlencode($doc_string);
+        $url = "http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=", $doc_string;
+    ?>
 </body>
 </html>
